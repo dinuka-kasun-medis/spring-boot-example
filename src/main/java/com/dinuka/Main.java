52b3c5c316891e52af278f680a8307e5eb3089ab
Dinuka Kasun <dinuka.kasunds@gmail.com>
2023-04-11 23:47:30 +0530

    Read customer from db

--- src/main/java/com/dinuka/Main.java
+++ src/main/java/com/dinuka/Main.java
@@ -3,27 +3,29 @@ package com.dinuka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetingResponse greeting() {
-        Person person = new Person("Dinuka", 27, 30_000);
-        GreetingResponse response = new GreetingResponse("Hello", List.of("Java", "Python", "JavaScripts"), person);
-        return response;
-    }
-
-    record Person(String name, int age, double savings) {
-    }
-
-    record GreetingResponse(String greet, List<String> favouriteLanguages, Person person) {
+    @GetMapping
+    public List<Customer> getCustomers()
+    {
+        return customerRepository.findAll();
     }
 }
