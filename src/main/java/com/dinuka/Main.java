a3c092df1577f38b542d9d918464f07227dfbb48
Dinuka Kasun <dinuka.kasunds@gmail.com>
2023-04-12 00:23:56 +0530

    CRUD

--- src/main/java/com/dinuka/Main.java
+++ src/main/java/com/dinuka/Main.java
@@ -2,11 +2,10 @@ package com.dinuka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
@@ -27,5 +26,39 @@ public class Main {
     public List<Customer> getCustomers()
     {
         return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(String name, String email, Integer age)
+    {
+
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request)
+    {
+        Customer customer = new Customer();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id)
+    {
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request)
+    {
+        Optional<Customer> result = customerRepository.findById(id);
+        if(result.isPresent()){
+            Customer customer = result.get();
+            customer.setName(request.name);
+            customer.setEmail(request.email);
+            customer.setAge(request.age);
+            customerRepository.saveAndFlush(customer);
+        }
     }
 }
